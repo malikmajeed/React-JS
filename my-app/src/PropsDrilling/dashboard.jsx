8dd58429d29ecf7afb08d0d28ab1fadbc20e4bb8
Malik Majeed <malikmajeedzia@gmail.com>
2025-07-28 18:09:28 +0500

    props Drilling

---
+++ my-app/src/PropsDrilling/dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LastChild from './lastChild'
+
+const Dashboard = ({isLoggedIn, handleLogIn, handleLogOut}) => {
+
+  console.log(`Dashboard component rendered & isLoggedIn = ${isLoggedIn}`);
+
+  return (
+    <div>
+        
+      <h1>Dashboard</h1>
+      {/* receiving from parant as handleLogin and passing to child as OnLogIn */}
+      <LastChild isLoggedIn={isLoggedIn} onLogIn={handleLogIn} onLogOut={handleLogOut} />
+     
+    </div>
+  );
+}
+
+export default Dashboard
