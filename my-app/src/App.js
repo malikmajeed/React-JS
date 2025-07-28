05480b06b6550751e5edaa6bc05eb93e982ba70d
Malik Majeed <malikmajeedzia@gmail.com>
2025-07-28 20:13:14 +0500

    files renamed for props drilling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-
-import Main from "./PropsDrilling/index.jsx";
+import Main from './propsDrilling/'
 
 function App() {
   return (
--- my-app/src/props_drilling/index.jsx
+++ my-app/src/props_drilling/index.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
-import HomePage from './homePage';
+import HomePage from './child_01';
 
 
 
 const Main = () => {
 
 const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//we'll pass thse props as index.jsx --> homPage --> Dashbard --> Last Child. 
+
 
 
 console.log(`Main component rendered & isLoggedIn = ${isLoggedIn}`);
